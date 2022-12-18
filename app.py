2caf7f9fcbc833228d87b6051089df32ff8594c2
Mahdi Maymandi <maymandinejad@gmail.com>
2022-12-17 20:35:06 -0500

    Initial Version

--- app.py
+++ app.py
@@ -1,9 +1,63 @@
+import json
 from mastodon import Mastodon
+import feedparser
+from cuttpy import Cuttpy
+import config
 
-#   Set up Mastodon
 mastodon = Mastodon(
-    access_token = 'test',
+    access_token = config.bot_token,
     api_base_url = 'https://mstdn.social/'
 )
 
-# mastodon.status_post("hello world!")
+def postArticles(source):
+    newsFeed = None
+    if (source == "NYT"):
+        newsFeed = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml")
+    
+    i = 0
+    while (i < config.total_posts_per_source):
+        entry = Entry(newsFeed.entries[i]["title"], newsFeed.entries[i]["summary"], newsFeed.entries[i]["link"], newsFeed.entries[i]["published"], "New York Times")
+        entry.postOnBot()
+        i += 1
+
+class Entry:
+    def __init__(self, title, summary, link, published, source):
+        self.title = title
+        self.summary = summary
+        self.link = link
+        self.published = published
+        self.source = source
+
+    def shortenURL(self):
+        shortener = Cuttpy(config.cuttly_token)
+        response = shortener.shorten(self.link)
+        self.shortURL = response.shortened_url
+    
+    def postOnBot(self):
+        if (not self.isEntryUnique()):
+            return False
+
+        self.recordEntry()
+        self.shortenURL()
+
+        message = self.title + " \nSource: " + self.source + "\nLink: " + self.shortURL
+        mastodon.status_post(message)
+
+        return True
+
+    def isEntryUnique(self):
+        f = open("entries.json")
+        data = json.loads(f.read())
+        for record in data:
+            if ((record['title'] == self.title or record['summary'] == self.summary or record['link'] == self.link) and record['published'] == self.published):
+                return False
+        return True
+    
+    def recordEntry(self):
+        f = open("entries.json")
+        data = json.loads(f.read())
+        data.append({"title": self.title, "summary": self.summary, "link": self.link, "published": self.published})
+        with open("entries.json", "w") as outfile:
+            json.dump(data, outfile)
+
+postArticles("NYT")
